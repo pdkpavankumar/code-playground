e2fa6a4cf44316b506c5ed99ab893d4298516e9f
pdkpavankumar <pavankumar8545@gmail.com>
2020-02-11 23:14:33 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app-header">
+        <center className="title"></center>
       </header>
+      <div className="content">
+        <div className="html title"> HTML </div>
+        <div className="javascript title"> Javascript </div>
+        <div className="css title"> CSS </div>
+      </div>
+      <div className="content">
+        <div className="html" id="html">
+        </div>
+        <div className="javascript" id="javascript">
+        </div>
+        <div className="css" id="css">
+        </div>
+      </div>
+      <iframe id="output" className="iframe" style={{border: 'none',width: '100%',height: 'calc(100vh - 340px)'}}></iframe>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import CodeMirror from 'codemirror/lib/codemirror.js';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/javascript/javascript.js';
+import 'codemirror/mode/htmlmixed/htmlmixed.js';
+import 'codemirror/mode/css/css.js';
+import 'codemirror/theme/blackboard.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +16,65 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+window.javascriptcode = CodeMirror(document.querySelector("#javascript"), {
+  value: `function buttonClick() {
+  alert('button clicked!');
+}`,
+  lineNumbers: true,
+  mode: "javascript",
+  theme: 'blackboard'
+});
+
+window.htmlcode = CodeMirror(document.querySelector("#html"), {
+  value: `<div class="content">
+  Hello World!
+  <button onclick="buttonClick()">click Me!</button>
+</div>`,
+  lineNumbers: true,
+  mode: "htmlmixed",
+  theme: 'blackboard'
+});
+
+window.csscode = CodeMirror(document.querySelector("#css"), {
+  value: ".content {\n\tcolor: grey;\n}",
+  lineNumbers: true,
+  mode: "css",
+  theme: 'blackboard'
+});
+
+function renderChanges() {
+  var css = window.csscode.getValue();
+  var html = window.htmlcode.getValue();
+  var js = window.javascriptcode.getValue();
+  var document = window.document.querySelector("#output").contentDocument;
+  var documentContents = `
+          <!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+            <style>
+              ${css}
+            </style>
+          </head>
+          <body>
+            ${html}
+            <script type="text/javascript">
+              ${js}
+            </script>
+          </body>
+          </html>`
+  document.open();
+  document.write(documentContents);
+  document.close();
+}
+
+renderChanges();
+
+window.javascriptcode.on('change', renderChanges);
+window.csscode.on('change', renderChanges);
+window.htmlcode.on('change', renderChanges);
